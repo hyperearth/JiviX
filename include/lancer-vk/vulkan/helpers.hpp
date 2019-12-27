dea4b4c4cbf5b5a68751f9e9bb7b3a3b37cab408
helix.d <helixd.2k18@gmail.com>
2019-12-27 16:19:49 +0800

    WIP concept 2, add pipeline structures

---
+++ include/lancer-vk/vulkan/helpers.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "./structures.hpp"
+
+namespace vkh {
+    
+    // TODO: Some Vookoo-like aggregators and helpers
+    // 1. Ray Tracing Pipeline and SBT helper
+    // 2. Descriptor Set Layout aggregator
+    // 3. Render Pass helper
+    // 4. Device and driver aggregate
+    // 5. Descriptor Set aggregate and helper
+
+};
--- include/lancer-vk/vulkan/structures.hpp
+++ include/lancer-vk/vulkan/structures.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <vulkan/vulkan.h>
 #include <glm/glm.hpp>
 
@@ -5,40 +6,229 @@
 #include "./vulkan/bitfields.hpp"
 
 namespace vkh {
-    
-#ifdef USE_GLM
+
+//#ifdef USE_GLM
     using VkExtent3D = glm::uvec3;
-#endif
+    using VkExtent2D = glm::uvec2;
+    using VkOffset3D = glm::ivec3;
+    using VkOffset2D = glm::ivec2;
+//#endif
+
+    #define STRUCT_OPERATORS(NAME)\
+        operator ::NAME&() { return reinterpret_cast<::NAME&>(*this); };\
+        operator const ::NAME&() const { return reinterpret_cast<const ::NAME&>(*this); };\
+        NAME& operator =( const ::NAME& info ) { reinterpret_cast<::NAME&>(*this) = info; return *this; };\
+        NAME& operator =( const NAME& info ) { reinterpret_cast<::NAME&>(*this) = reinterpret_cast<const ::NAME&>(info); return *this; };
 
     // 
     typedef struct VkBufferCreateInfo {
-        VkStructureType     sType                       = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-        const void*         pNext                       = nullptr;
-        VkBufferCreateFlags flags;
-        VkDeviceSize        size                        = 4u;
-        VkBufferUsageFlags  usage;
-        VkSharingMode       sharingMode;                = VK_SHARING_MODE_EXCLUSIVE;
-        uint32_t            queueFamilyIndexCount       = 0u;
-        const uint32_t*     pQueueFamilyIndices         = nullptr;
+        VkStructureType     sType                   = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        const void*         pNext                   = nullptr;
+        VkBufferCreateFlags flags                   = {};
+        VkDeviceSize        size                    = 4u;
+        VkBufferUsageFlags  usage                   = {};
+        VkSharingMode       sharingMode;            = VK_SHARING_MODE_EXCLUSIVE;
+        uint32_t            queueFamilyIndexCount   = 0u;
+        const uint32_t*     pQueueFamilyIndices     = nullptr;
+
+        STRUCT_OPERATORS(VkBufferCreateInfo)
     } VkBufferCreateInfo;
 
     // 
     typedef struct VkImageCreateInfo {
         VkStructureType          sType                  = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
         const void*              pNext                  = nullptr;
-        VkImageCreateFlags       flags                  ;
+        VkImageCreateFlags       flags                  = {};
         VkImageType              imageType              = VK_IMAGE_TYPE_1D;
         VkFormat                 format                 = VK_FORMAT_R8G8_UNORM;
-        VkExtent3D               extent                 = {1u,1u};
+        VkExtent3D               extent                 = {1u,1u,1u};
         uint32_t                 mipLevels              = 1u;
         uint32_t                 arrayLayers            = 1u;
         VkSampleCountFlagBits    samples                = VK_SAMPLE_COUNT_1_BIT;
         VkImageTiling            tiling                 = VK_IMAGE_TILING_OPTIMAL;
-        VkImageUsageFlags        usage                  ;
+        VkImageUsageFlags        usage                  = {};
         VkSharingMode            sharingMode            = VK_SHARING_MODE_EXCLUSIVE;
         uint32_t                 queueFamilyIndexCount  = 0u;
         const uint32_t*          pQueueFamilyIndices    = nullptr;
         VkImageLayout            initialLayout          = VK_IMAGE_LAYOUT_UNDEFINED;
+
+        STRUCT_OPERATORS(VkImageCreateInfo)
     } VkImageCreateInfo;
+
+    // 
+    typedef struct VkPipelineInputAssemblyStateCreateInfo {
+        VkStructureType                                 sType                   = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        const void*                                     pNext                   = nullptr;
+        VkPipelineInputAssemblyStateCreateFlags         flags                   = {};
+        VkPrimitiveTopology                             topology                = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+        VkBool32                                        primitiveRestartEnable  = false;
+
+        STRUCT_OPERATORS(VkPipelineInputAssemblyStateCreateInfo)
+    } VkPipelineInputAssemblyStateCreateInfo;
+
+    // 
+    typedef struct VkPipelineVertexInputStateCreateInfo {
+        VkStructureType                                 sType                           = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+        const void*                                     pNext                           = nullptr;
+        VkPipelineVertexInputStateCreateFlags           flags                           = {};
+        uint32_t                                        vertexBindingDescriptionCount   = 0u;
+        const VkVertexInputBindingDescription*          pVertexBindingDescriptions      = nullptr;
+        uint32_t                                        vertexAttributeDescriptionCount = 0u;
+        const VkVertexInputAttributeDescription*        pVertexAttributeDescriptions    = nullptr;
+
+        STRUCT_OPERATORS(VkPipelineVertexInputStateCreateInfo)
+    } VkPipelineVertexInputStateCreateInfo;
+
+    // 
+    typedef struct VkPipelineTessellationStateCreateInfo {
+        VkStructureType                           sType                 = VK_STRUCTURE_TYPE_PIPELINE_TESSELLATION_STATE_CREATE_INFO;
+        const void*                               pNext                 = nullptr;
+        VkPipelineTessellationStateCreateFlags    flags                 = {};
+        uint32_t                                  patchControlPoints    = 0u;
+
+        STRUCT_OPERATORS(VkPipelineTessellationStateCreateInfo)
+    } VkPipelineTessellationStateCreateInfo;
+
+    // 
+    typedef struct VkPipelineViewportStateCreateInfo {
+        VkStructureType                       sType         = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+        const void*                           pNext         = nullptr;
+        VkPipelineViewportStateCreateFlags    flags         = {};
+        uint32_t                              viewportCount = 1u;
+        const VkViewport*                     pViewports    = nullptr;
+        uint32_t                              scissorCount  = 1u;
+        const VkRect2D*                       pScissors     = nullptr;
+
+        STRUCT_OPERATORS(VkPipelineViewportStateCreateInfo)
+    } VkPipelineViewportStateCreateInfo;
+
+    // 
+    typedef struct VkPipelineRasterizationStateCreateInfo {
+        VkStructureType                            sType                    = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+        const void*                                pNext                    = nullptr;
+        VkPipelineRasterizationStateCreateFlags    flags                    = {};
+        VkBool32                                   depthClampEnable         = false;
+        VkBool32                                   rasterizerDiscardEnable  = true;
+        VkPolygonMode                              polygonMode              = VK_POLYGON_MODE_FILL;
+        VkCullModeFlags                            cullMode                 = {};
+        VkFrontFace                                frontFace                = VK_FRONT_FACE_COUNTER_CLOCKWISE ;
+        VkBool32                                   depthBiasEnable          = false;
+        float                                      depthBiasConstantFactor  = 0.f;
+        float                                      depthBiasClamp           = 0.f;
+        float                                      depthBiasSlopeFactor     = 0.f;
+        float                                      lineWidth                = 1.f;
+
+        STRUCT_OPERATORS(VkPipelineRasterizationStateCreateInfo)
+    } VkPipelineRasterizationStateCreateInfo;
+
+    //
+    typedef struct VkPipelineMultisampleStateCreateInfo {
+        VkStructureType                          sType                  = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+        const void*                              pNext                  = nullptr;
+        VkPipelineMultisampleStateCreateFlags    flags                  = {};
+        VkSampleCountFlagBits                    rasterizationSamples   = VK_SAMPLE_COUNT_1_BIT;
+        VkBool32                                 sampleShadingEnable    = false;
+        float                                    minSampleShading       = 0.f;
+        const VkSampleMask*                      pSampleMask            = nullptr;
+        VkBool32                                 alphaToCoverageEnable  = false;
+        VkBool32                                 alphaToOneEnable       = false;
+
+        STRUCT_OPERATORS(VkPipelineMultisampleStateCreateInfo)
+    } VkPipelineMultisampleStateCreateInfo;
+
+    // 
+    typedef struct VkPipelineDepthStencilStateCreateInfo {
+        VkStructureType                           sType                 = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+        const void*                               pNext                 = nullptr;
+        VkPipelineDepthStencilStateCreateFlags    flags                 = {};
+        VkBool32                                  depthTestEnable       = false;
+        VkBool32                                  depthWriteEnable      = false;
+        VkCompareOp                               depthCompareOp        = VK_COMPARE_OP_LESS_OR_EQUAL;
+        VkBool32                                  depthBoundsTestEnable = false;
+        VkBool32                                  stencilTestEnable     = false;
+        VkStencilOpState                          front                 = VK_STENCIL_OP_KEEP;
+        VkStencilOpState                          back                  = VK_STENCIL_OP_KEEP;
+        float                                     minDepthBounds        = 0.f;
+        float                                     maxDepthBounds        = 1.f;
+
+        STRUCT_OPERATORS(VkPipelineDepthStencilStateCreateInfo)
+    } VkPipelineDepthStencilStateCreateInfo;
+
+    // 
+    typedef struct VkPipelineColorBlendStateCreateInfo {
+        VkStructureType                               sType            = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+        const void*                                   pNext            = nullptr;
+        VkPipelineColorBlendStateCreateFlags          flags            = {};
+        VkBool32                                      logicOpEnable    = false;
+        VkLogicOp                                     logicOp          = VK_LOGIC_OP_SET;
+        uint32_t                                      attachmentCount  = 0u;
+        const VkPipelineColorBlendAttachmentState*    pAttachments     = nullptr;
+        glm::vec4                                     blendConstants   = {1.f,1.f,1.f,1.f};
+        //float                                         blendConstants[4];
+
+        STRUCT_OPERATORS(VkPipelineColorBlendStateCreateInfo)
+    } VkPipelineColorBlendStateCreateInfo;
+
+    // 
+    typedef struct VkPipelineDynamicStateCreateInfo {
+        VkStructureType                      sType              = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+        const void*                          pNext              = nullptr;
+        VkPipelineDynamicStateCreateFlags    flags              = {};
+        uint32_t                             dynamicStateCount  = 0u;
+        const VkDynamicState*                pDynamicStates     = nullptr;
+
+        STRUCT_OPERATORS(VkPipelineDynamicStateCreateInfo)
+    } VkPipelineDynamicStateCreateInfo;
+
+    //
+    typedef struct VkPipelineShaderStageCreateInfo {
+        VkStructureType                     sType               = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        const void*                         pNext               = nullptr;
+        VkPipelineShaderStageCreateFlags    flags               = {};
+        VkShaderStageFlagBits               stage               = VK_SHADER_STAGE_COMPUTE_BIT;
+        VkShaderModule                      module              = VK_NULL_HANDLE;
+        const char*                         pName               = nullptr;
+        const VkSpecializationInfo*         pSpecializationInfo = nullptr;
+
+        STRUCT_OPERATORS(VkPipelineShaderStageCreateInfo)
+    } VkPipelineShaderStageCreateInfo;
+
+    // 
+    typedef struct VkGraphicsPipelineCreateInfo {
+        VkStructureType                                  sType                  = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+        const void*                                      pNext                  = nullptr;
+        VkPipelineCreateFlags                            flags                  = {};
+        uint32_t                                         stageCount             = 0u;
+        const VkPipelineShaderStageCreateInfo*           pStages                = nullptr;
+        const VkPipelineVertexInputStateCreateInfo*      pVertexInputState      = nullptr;
+        const VkPipelineInputAssemblyStateCreateInfo*    pInputAssemblyState    = nullptr;
+        const VkPipelineTessellationStateCreateInfo*     pTessellationState     = nullptr;
+        const VkPipelineViewportStateCreateInfo*         pViewportState         = nullptr 
+        const VkPipelineRasterizationStateCreateInfo*    pRasterizationState    = nullptr;
+        const VkPipelineMultisampleStateCreateInfo*      pMultisampleState      = nullptr;
+        const VkPipelineDepthStencilStateCreateInfo*     pDepthStencilState     = nullptr;
+        const VkPipelineColorBlendStateCreateInfo*       pColorBlendState       = nullptr;
+        const VkPipelineDynamicStateCreateInfo*          pDynamicState          = nullptr;
+        VkPipelineLayout                                 layout                 = VK_NULL_HANDLE;
+        VkRenderPass                                     renderPass             = VK_NULL_HANDLE;
+        uint32_t                                         subpass                = 0u;
+        VkPipeline                                       basePipelineHandle     = VK_NULL_HANDLE;
+        int32_t                                          basePipelineIndex      = 0;
+
+        STRUCT_OPERATORS(VkGraphicsPipelineCreateInfo)
+    } VkGraphicsPipelineCreateInfo;
+
+    // 
+    typedef struct VkComputePipelineCreateInfo {
+        VkStructureType                    sType                = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO;
+        const void*                        pNext                = nullptr;
+        VkPipelineCreateFlags              flags                = {};
+        VkPipelineShaderStageCreateInfo    stage                = {};
+        VkPipelineLayout                   layout               = VK_NULL_HANDLE;
+        VkPipeline                         basePipelineHandle   = VK_NULL_HANDLE;
+        int32_t                            basePipelineIndex    = 0;
+
+        STRUCT_OPERATORS(VkComputePipelineCreateInfo)
+    } VkComputePipelineCreateInfo;
 
 };
