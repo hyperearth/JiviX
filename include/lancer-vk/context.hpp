21d7584a775299be82e9a2abdb99285c83ecb6fb
helix.d <helixd.2k18@gmail.com>
2020-01-03 14:42:30 +0800

    WIP context

--- include/lancer-vk/context.hpp
+++ include/lancer-vk/context.hpp
@@ -30,8 +30,33 @@ namespace lancer {
         const vk::Framebuffer& refFramebuffer() const { return framebuffer; };
 
         // 
-        
+        std::shared_ptr<Context> createFramebuffers() {
 
+            return shared_from_this();
+        };
+
+        // 
+        std::shared_ptr<Context> createDescriptorSetLayouts() { // 
+            for (uint32_t b=0u;b<8u;b++) { // For Ray Tracers
+                meshDataDescriptorSetLayoutHelper.pushBinding(vkh::VkDescriptorSetLayoutBinding{ .binding = b, .descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER     , .descriptorCount =   64u, .stageFlags = { .eCompute = 1, .eRaygen = 1, .eClosestHit = 1 } },vkh::VkDescriptorBindingFlagsEXT{.ePartiallyBound = 1});
+            };
+
+            //
+            bindingsDescriptorSetLayoutHelper.pushBinding(vkh::VkDescriptorSetLayoutBinding{ .binding = 0u, .descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER        , .descriptorCount =   1u, .stageFlags = { .eCompute = 1, .eRaygen = 1, .eClosestHit = 1 } },vkh::VkDescriptorBindingFlagsEXT{.ePartiallyBound = 1});
+            bindingsDescriptorSetLayoutHelper.pushBinding(vkh::VkDescriptorSetLayoutBinding{ .binding = 1u, .descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER        , .descriptorCount =   1u, .stageFlags = { .eCompute = 1, .eRaygen = 1, .eClosestHit = 1 } },vkh::VkDescriptorBindingFlagsEXT{.ePartiallyBound = 1});
+
+            // 
+            samplingDescriptorSetLayoutHelper.pushBinding(vkh::VkDescriptorSetLayoutBinding{ .binding = 0u, .descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE         , .descriptorCount =   8u, .stageFlags = { .eCompute = 1, .eRaygen = 1, .eClosestHit = 1 } },vkh::VkDescriptorBindingFlagsEXT{.ePartiallyBound = 1});
+            rayTraceDescriptorSetLayoutHelper.pushBinding(vkh::VkDescriptorSetLayoutBinding{ .binding = 0u, .descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE         , .descriptorCount =   8u, .stageFlags = { .eCompute = 1, .eRaygen = 1, .eClosestHit = 1 } },vkh::VkDescriptorBindingFlagsEXT{.ePartiallyBound = 1});
+            rayTraceDescriptorSetLayoutHelper.pushBinding(vkh::VkDescriptorSetLayoutBinding{ .binding = 1u, .descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER        , .descriptorCount =   8u, .stageFlags = { .eCompute = 1, .eRaygen = 1, .eClosestHit = 1 } },vkh::VkDescriptorBindingFlagsEXT{.ePartiallyBound = 1});
+
+            // 
+            materialDescriptorSetLayoutHelper.pushBinding(vkh::VkDescriptorSetLayoutBinding{ .binding = 0u, .descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, .descriptorCount = 128u, .stageFlags = { .eCompute = 1, .eRaygen = 1, .eClosestHit = 1 } },vkh::VkDescriptorBindingFlagsEXT{.ePartiallyBound = 1});
+            materialDescriptorSetLayoutHelper.pushBinding(vkh::VkDescriptorSetLayoutBinding{ .binding = 1u, .descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER        , .descriptorCount =   8u, .stageFlags = { .eCompute = 1, .eRaygen = 1, .eClosestHit = 1 } },vkh::VkDescriptorBindingFlagsEXT{.ePartiallyBound = 1});
+
+            return shared_from_this();
+        };
+        
     protected: // 
         vk::Rect2D scissor = {};
         vk::Viewport viewport = {};
@@ -39,12 +64,23 @@ namespace lancer {
         vk::Framebuffer framebuffer = {};
 
         // 
+        std::array<vkt::ImageRegion,8u> frameImages = {};
+        vkt::ImageRegion depthImage = {};
+
+        // 
         vk::PipelineLayout unifiedPipelineLayout = {};
         vk::DescriptorSetLayout materialDescriptorSetLayout = {}; // Material Descriptions
         vk::DescriptorSetLayout rayTraceDescriptorSetLayout = {}; // Ray-Traced Data
         vk::DescriptorSetLayout meshDataDescriptorSetLayout = {}; // Packed Mesh Data (8-bindings)
-        vk::DescriptorSetLayout samplingDescriptorSetLayout = {}; // Framebuffers and Samples
+        vk::DescriptorSetLayout samplingDescriptorSetLayout = {}; // Framebuffers and Samples (Diffuse, Path-Tracing and Reprojection)
         vk::DescriptorSetLayout bindingsDescriptorSetLayout = {}; // Bindings, Attributes Descriptions
+
+        // 
+        vkh::VsDescriptorSetLayoutCreateInfoHelper materialDescriptorSetLayoutHelper = {};
+        vkh::VsDescriptorSetLayoutCreateInfoHelper rayTraceDescriptorSetLayoutHelper = {};
+        vkh::VsDescriptorSetLayoutCreateInfoHelper meshDataDescriptorSetLayoutHelper = {};
+        vkh::VsDescriptorSetLayoutCreateInfoHelper samplingDescriptorSetLayoutHelper = {};
+        vkh::VsDescriptorSetLayoutCreateInfoHelper bindingsDescriptorSetLayoutHelper = {};
 
         // 
         std::shared_ptr<Driver> driver = {};
